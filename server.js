69bd448ff0cf6ebbe0c547304e2b42d68397585f
Min Sang Sohng <minsohng@gmail.com>
2019-04-09 18:46:30 +0000

    printing values in urls object to browser

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
-var express = require("express");
-var app = express();
-var PORT = 8080;
+const express = require("express");
+const app = express();
+const PORT = 8080;
 
-app.set("view engine", "ejs");
+const urlDatabase = {
+  'b2xVn2': "http://www.lighthouselabs.ca",
+  '9sm5xK': "http://www.google.com"
+};
 
+app.set('view engine', 'ejs');
+
+app.get('/', (request, response) => {
+  response.send('Hello!');
+});
+
+app.get('/urls', (request, response) => {
+  let templateVars = { urls: urlDatabase };
+  response.render("urls_index", templateVars);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
