a6fc2abd1a70336c9f21d9836738a7c4b956ebec
Min Sang Sohng <minsohng@gmail.com>
2019-04-09 20:37:21 +0000

    added body-parser package

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const urlDatabase = {
   '9sm5xK': "http://www.google.com"
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set('view engine', 'ejs');
 
 app.get('/', (request, response) => {
