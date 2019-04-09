fcf86f026b62ed6ae3b3b3635a75c00073015371
Min Sang Sohng <minsohng@gmail.com>
2019-04-09 20:34:04 +0000

    create urls_new page and route handler

--- server.js
+++ server.js
@@ -15,16 +15,22 @@ app.get('/', (request, response) => {
 
 app.get('/urls', (request, response) => {
   const templateVars = { urls: urlDatabase };
-  response.render("urls_index", templateVars);
+  response.render('urls_index', templateVars);
 });
 
-app.get("/urls/:shortURL", (request, response) => {
+app.get('/urls/new', (request, response) => {
+  response.render('urls_new')
+});
+
+app.get('/urls/:shortURL', (request, response) => {
   const templateVars = {
     shortURL: request.params.shortURL,
     longURL: urlDatabase[request.params.shortURL]
   }; //
-  response.render("urls_show", templateVars);
+  response.render('urls_show', templateVars);
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
